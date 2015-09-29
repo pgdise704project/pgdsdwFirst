514aab08b4c4a763f27eddb688623bb85eca11d7
pgdswd <pgdswd@lab-foss-pc>
2015-09-29 15:01:35 +0200

    First commit Second commit Third commit

--- First/src/pgdswdfirst/First.java
+++ First/src/pgdswdfirst/First.java
@@ -6,7 +6,9 @@ package pgdswdfirst;
  */
 public class First {
     public static void main(String[] args) {
-        System.out.println("This is my forst commit");
+        System.out.println("This is my first commit");
+        
+        System.out.println("Another line of commit");
     }
             
 
